e0d1e162456ee5c470c2eb917eb37cd9bb72cf69
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-10-19 01:38:58 +0200

    axios change url for data

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function fetchData(dataTypes) {
   try {
-    const response = await axios.get("./public/data/data.json");
+    const response = await axios.get("./data/data.json");
     return response.data[dataTypes];
   } catch (error) {
     console.error("Chyba při načítání dat: ", error);
